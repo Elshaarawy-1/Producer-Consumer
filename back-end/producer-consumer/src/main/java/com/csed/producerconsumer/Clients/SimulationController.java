b598081c26603f6e2ecf6e5396fea5c4bb6713d7
احمد يوسف صبحى محمد الجعرانى <108885178+ahmedyoussefg@users.noreply.github.com>
2024-01-19 18:48:24 +0200

    Delete unnecessary classes and codes

--- back-end/producer-consumer/src/main/java/com/csed/producerconsumer/Clients/SimulationController.java
+++ back-end/producer-consumer/src/main/java/com/csed/producerconsumer/Clients/SimulationController.java
@@ -1,25 +1,10 @@
 package com.csed.producerconsumer.Clients;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/simulation")
 public class SimulationController {
-
-    private final SimulationService simulationService;
-
-    @Autowired
-    public SimulationController(SimulationService simulationService) {
-        this.simulationService = simulationService;
-    }
-
-    @PostMapping("/start")
-    public void startSimulation() {
-        simulationService.startSimulation();
-    }
-    @PostMapping("/stop")
-    public void stopSimulation() {
-        simulationService.stopSimulation();
-    }
+    // TODO : Basic Endpoints (start/stop/replay simulation)
+    // Start Simulation should take number of products from front-end
 }
